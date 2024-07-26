9ab195b9643117dbf2b0c900cf076f70a076abad
yiyun-sj <samyiyunjia@gmail.com>
2024-07-25 19:34:49 -0700

    seo updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,52 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Yiyun Jia',
-  description: 'Yiyun Jia Portfolio',
+  metadataBase: new URL('https://yiyun-sj.github.io/yap'),
+  title: {
+    default: 'Yap',
+    template: '%s | Yap',
+  },
+  description: 'Yap - Rate Anything!',
+  generator: 'Next.js',
+  applicationName: 'Yap',
+  keywords: ['Yap', 'Rate', 'Social', 'Media', 'App', 'ios'],
+  authors: [{ name: 'Yiyun Jia', url: 'https://yiyun-sj.github.io' }],
+  creator: 'Yiyun Jia',
+  publisher: 'Yiyun Jia',
+  openGraph: {
+    title: {
+      default: 'Yap',
+      template: '%s | Yap',
+    },
+    description: 'Yap - Rate Anything!',
+    url: 'https://yiyun-sj.github.io/yap',
+    siteName: 'Yap',
+    images: [
+      {
+        url: '/icon.png',
+        width: 1000,
+        height: 1000,
+      },
+    ],
+    locale: 'en_CA',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+  icons: {
+    icon: '/icon.png',
+  },
+  itunes: {
+    appId: '6569233860',
+    appArgument: 'yapapp://',
+  },
 }
 
 export default function RootLayout({
@@ -16,10 +60,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <meta
-        name='apple-itunes-app'
-        content='app-id=6569233860, app-argument=yapapp://'
-      />
       <body className='flex min-h-screen flex-col p-4'>{children}</body>
     </html>
   )
